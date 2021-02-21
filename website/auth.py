3d29edb2fe02c5622fe5fd8e60558ebb7415947a
totalchest <13sergey78@mail.ru>
2021-02-21 19:44:41 +0300

    Add sqlite database, home page and js function for note delete

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,36 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-
+from . import db
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Вы успешно вошли!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Неверный пароль.', category='error')
+        else:
+            flash('Пользователя не существует.', category='error')
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return "<h1>LOGOUT</h1>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route("/signup", methods=["GET", "POST"])
@@ -20,15 +39,23 @@ def signup():
         email = request.form.get('email')
         name = request.form.get('name')
         password1 = request.form.get('password1')
-        password2 = request.form.get('pasword2')
+        password2 = request.form.get('password2')
 
-        if len(name) < 3:
-            flash("Имя должно быть длинее, чем 2 символа.", category="error")
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Пользователь с таким электронным адресом уже существует.', category='error')
+        elif len(name) < 3:
+            flash('Имя должно быть длинее, чем 2 символа.', category='error')
         elif len(password1) < 5:
-            flash("Пароль должен быть длинее, чем 4 символа.", category="error")
+            flash('Пароль должен быть длинее, чем 4 символа.', category='error')
         elif password1 != password2:
-            flash("Пароли не совпадают.", category="error")
+            flash('Пароли не совпадают.', category='error')
         else:
-            flash("Аккаунт создан!", category="success")
+            new_user = User(email=email, name=name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Аккаунт создан!', category='success')
+            login_user(new_user, remember=True)
+            return redirect(url_for('views.home'))
 
-    return render_template("signup.html")
+    return render_template("signup.html", user=current_user)
--- website/views.py
+++ website/views.py
@@ -1,10 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 
 views = Blueprint('views', __name__)
 
 
-@views.route("/")
+@views.route("/", methods=["GET", "POST"])
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        note = request.form.get('note')
+        new_note = Note(user_id=current_user.id, data=note)
+        db.session.add(new_note)
+        db.session.commit()
+    return render_template("home.html", user=current_user)
 
+
+@views.route("/delete-note", methods=["POST"])
+def delete_note():
+    note_id = json.loads(request.data)['noteId']
+    note = Note.query.get(note_id)
+    if note and current_user.is_authenticated:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonfy({})
